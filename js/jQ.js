54c0f194584179891a821c30eb03feba1322a314
Laxus-djm <73738212+Laxus-djm@users.noreply.github.com>
2021-01-29 01:13:27 +0000

    hi

---
+++ js/jQ.js
@@ -0,0 +1,11 @@
+
+$(function(){
+    $("div").animate({
+        top:'100px'
+    },800,function () { $("div").animate({ height:400},800,function () { $("h2").show(800,function(){$("p").show(800,function(){$("input").show(800,function(){$("button").show(800)})
+})})},)
+    })
+})
+
+
+
